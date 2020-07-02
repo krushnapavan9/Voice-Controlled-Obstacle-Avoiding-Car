50deb3efeb71f319a12751300372c32279e3b877
krushnapavan <35088826+krushnapavan9@users.noreply.github.com>
2020-07-02 07:54:25 +0530

    Add files via upload

---
+++ sketch_nov28c.ino
@@ -0,0 +1,126 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10, 11); //TX, RX respetively
+String readvoice;
+int in3=3;
+int in4=4;
+int in1=6;
+int in2=5;
+
+int trigPin=9;
+int echoPin=12;
+long duration, cm, inches;
+void setup() {
+ BT.begin(9600);
+ Serial.begin(9600);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  delay(100);
+  Serial.println();
+
+  if(cm <= 10){
+
+     digitalWrite (3, LOW);
+   digitalWrite (4, LOW);
+   digitalWrite (5, LOW);
+   digitalWrite (6, LOW);
+   delay (100);
+   }
+  
+  while (BT.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable 
+  char c = BT.read(); //Conduct a serial read
+  readvoice += c; //build the string- "forward", "reverse", "left" and "right"
+  }  
+  if (readvoice.length() > 0) {
+    Serial.println(readvoice); 
+
+  if(readvoice == "forward") 
+  {
+    digitalWrite(3, HIGH);
+    digitalWrite (4, HIGH);
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
+    delay(100);
+  } 
+  
+  else if(readvoice == "back" || readvoice == "reverse" ) 
+  {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6,HIGH);
+    delay(100);
+  }
+  
+  else if (readvoice == "right" )
+  {
+    digitalWrite (3,HIGH);
+    digitalWrite (4,LOW);
+    digitalWrite (5,LOW);
+    digitalWrite (6,LOW);
+   delay (500);
+   digitalWrite(3, HIGH);
+    digitalWrite (4, HIGH);
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
+    delay(100);
+  }
+  
+ else if ( readvoice == "left")
+ {
+   digitalWrite (3, LOW);
+   digitalWrite (4, HIGH);
+   digitalWrite (5, LOW);
+   digitalWrite (6, LOW);
+   delay (1000);
+  digitalWrite(3, HIGH);
+    digitalWrite (4, HIGH);
+    digitalWrite(5,LOW);
+    digitalWrite(6,LOW);
+    delay(100);
+ }
+ 
+ else if (readvoice == "stop")
+ {
+   digitalWrite (3, LOW);
+   digitalWrite (4, LOW);
+   digitalWrite (5, LOW);
+   digitalWrite (6, LOW);
+   delay (100);
+ }
+ 
+   
+
+readvoice="";}
+
+
+
+} //Reset the variable
+
